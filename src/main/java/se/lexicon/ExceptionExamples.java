bf7f7ba76a2bb4c432a03e60a129c585010f4dc8
Fatemeh <f.khalleghi.777@gmail.com>
2024-12-07 21:59:36 +0100

    Checked (Compile Time) Exception

--- src/main/java/se/lexicon/ExceptionExamples.java
+++ src/main/java/se/lexicon/ExceptionExamples.java
@@ -1,10 +1,22 @@
 package se.lexicon;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ExceptionExamples {
     public static void main(String[] args) {
 
         // Unchecked (Runtime) Exception
-        int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println(numbers[10]);
+        // int[] numbers = {1, 2, 3, 4, 5};
+        // System.out.println(numbers[10]);
+
+        // Checked (Compile Time) Exception
+        Path filePath = Paths.get("folder/TextHere.txt");
+        BufferedReader reader = Files.newBufferedReader(filePath);
+
+
     }
 }
