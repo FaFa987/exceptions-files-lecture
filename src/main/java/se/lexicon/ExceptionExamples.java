6307becdeb49dfba4f5df4ba8eceec9f2744a9bb
Fatemeh <f.khalleghi.777@gmail.com>
2024-12-07 21:45:10 +0100

    Unchecked (Runtime) Exception

---
+++ src/main/java/se/lexicon/ExceptionExamples.java
@@ -0,0 +1,10 @@
+package se.lexicon;
+
+public class ExceptionExamples {
+    public static void main(String[] args) {
+
+        // Unchecked (Runtime) Exception
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(numbers[10]);
+    }
+}
